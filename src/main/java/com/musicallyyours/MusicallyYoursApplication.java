44db134b06cd2338b28c7fcbb7cfa7c5f3555a20
ManjunathMGowda92 <manjunathcs052@gmail.com>
2020-04-17 20:38:45 +0530

    Initial Commit for Musically-Yours

---
+++ src/main/java/com/musicallyyours/MusicallyYoursApplication.java
@@ -0,0 +1,13 @@
+package com.musicallyyours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicallyYoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicallyYoursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicallyyours/MusicallyYoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicallyyours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicallyYoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
